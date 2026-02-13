349759e53ea42f6be1f391cd7766168bee43e10a
ohakoglu <ohakoglu@gmail.com>
2026-02-13 18:35:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,34 @@
-const CACHE = "perinin-koalasi-v02";
+const CACHE = "perinin-koalasi-v04";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./manifest.json",
+  "./sw.js",
   "./js/app.js",
   "./js/state.js",
   "./js/storage.js",
   "./js/koala.js",
+
+  "./assets/koala/fx/blink_lids.png",
   "./assets/koala/base/base_mint.png",
   "./assets/koala/base/base_lavender.png",
-  "./assets/koala/base/base_sky.png",
-  "./assets/koala/hats/hat_flower.png",
-  "./assets/koala/hats/hat_beanie.png",
-  "./assets/koala/hats/hat_sun.png",
-  "./assets/koala/hats/hat_ribbon.png",
-  "./assets/koala/hats/hat_crown.png",
-  "./assets/koala/neck/neck_scarf_red.png",
-  "./assets/koala/neck/neck_bow_blue.png",
-  "./assets/koala/neck/neck_leaf_necklace.png",
-  "./assets/koala/hand/hand_star.png",
-  "./assets/koala/hand/hand_book.png",
-  "./assets/koala/hand/hand_leaf.png"
+  "./assets/koala/base/base_sky.png"
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then((c) => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
